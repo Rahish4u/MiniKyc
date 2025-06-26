a80fd0065a17d82f2fd8ede66f645ec83a98e9a0
Rahish4u <rahishali0591@gmail.com>
2025-06-26 11:55:42 +0530

    updateStatus issue

--- Server/routes/kycRoute.js
+++ Server/routes/kycRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createKYC, getAllKYC } = require("../controllers/controller");
+const { createKYC, getAllKYC, updateStatus  } = require("../controllers/controller");
 
 router.post("/kyc", createKYC);
 router.get("/kyc", getAllKYC);
